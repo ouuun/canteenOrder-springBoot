1292085077d10a9ce4368dd7130ce587a18b1a97
范家铭 <954574569@qq.com>
2022-09-08 21:11:26 +0800

    first commit

---
+++ src/main/java/com/fjm/canteenOrder/CanteenOrderApplication.java
@@ -0,0 +1,13 @@
+package com.fjm.canteenOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fjm/canteenOrder/CanteenOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjm.canteenOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
